6f1390d5e74fc2cc93a539a188a8b01625c61153
Eduardo Kraus <kraus@eduardokraus.com>
2024-11-09 17:16:51 -0300

    Javascript coding style problems

--- amd/src/copy.js
+++ amd/src/copy.js
@@ -1,18 +1,18 @@
 define(["jquery"], function($) {
     return {
-        init : function(copytext) {
+        init: function(copytext) {
             $(".section .activity").each(function(id, element) {
                 var $element = $(element);
 
-                var mod_id = $element.attr("data-id");
+                var modId = $element.attr("data-id");
                 var name = encodeURIComponent($element.find(".activity-item").attr("data-activityname"));
                 var urlreturn = encodeURIComponent(location.href);
                 $element.find(".editing_delete")
-                    .before(`<a href="/local/copy/copy.php?module=${mod_id}&name=${name}&returnurl=${urlreturn}"
+                    .before(`<a href="/local/copy/copy.php?module=${modId}&name=${name}&returnurl=${urlreturn}"
                                 class="dropdown-item menu-action cm-edit-action">
                                  <i class="icon fa fa-copy fa-fw"></i>
                                  <span class="menu-action-text">${copytext}</span>
-                             </a>`)
+                             </a>`);
             });
         }
     };
--- amd/src/paste.js
+++ amd/src/paste.js
@@ -1,25 +1,28 @@
 define(["jquery"], function($) {
     return {
-        init : function(courseid, pastetext) {
-            $(".course-section").each(function(id, course_section) {
-                var $coursesection = $(course_section);
+        init: function(courseid, pastetext) {
+            $(".course-section").each(function(id, courseSection) {
+                var $coursesection = $(courseSection);
 
-                var section_id = $coursesection.attr("data-id");
+                var sectionId = $coursesection.attr("data-id");
 
                 $coursesection.find(".activity").each(function(id, activity) {
                     var $activity = $(activity);
-                    var mod_id = $activity.attr("data-id");
+                    var modId = $activity.attr("data-id");
 
                     var urlreturn = encodeURIComponent(location.href);
+                    var url = `${M.cfg.wwwroot}/local/copy/paste.php?courseid=${courseid}&section=${sectionId}` +
+                        `&beforemodule=${modId}&returnurl=${urlreturn}`;
                     $activity.find(".divider")
                         .addClass("d-flex")
                         .addClass("align-items-center")
                         .append(`&nbsp;&nbsp;
-                            <form action="/local/copy/paste.php?courseid=${courseid}&section=${section_id}&beforemodule=${mod_id}&returnurl=${urlreturn}" method="post">
+                            <form action="${url}" 
+                                  method="post">
                                 <button class="btn btn-link" 
                                         style="width:auto;border-radius:14px;" 
                                         type="submit">&nbsp;&nbsp;${pastetext}&nbsp;&nbsp;</button>
-                            </form>`)
+                            </form>`);
                 });
             });
         }
