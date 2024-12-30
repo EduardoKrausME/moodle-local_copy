38e7539f2625c6e1868ea037056ce6a1babb4e4f
Eduardo Kraus <kraus@eduardokraus.com>
2024-12-29 21:43:25 -0300

    Broken URLs #1

--- amd/src/copy.js
+++ amd/src/copy.js
@@ -8,7 +8,7 @@ define(["jquery"], function($) {
                 var name = encodeURIComponent($element.find(".activity-item").attr("data-activityname"));
                 var urlreturn = encodeURIComponent(location.href);
                 $element.find(".editing_delete")
-                    .before(`<a href="/local/copy/copy.php?module=${modId}&name=${name}&returnurl=${urlreturn}"
+                    .before(`<a href="${M.cfg.wwwroot}/local/copy/copy.php?module=${modId}&name=${name}&returnurl=${urlreturn}"
                                 class="dropdown-item menu-action cm-edit-action">
                                  <i class="icon fa fa-copy fa-fw"></i>
                                  <span class="menu-action-text">${copytext}</span>
