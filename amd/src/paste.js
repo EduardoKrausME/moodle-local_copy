616bc027f6b518514746ec7fface26e4fe0e7640
Eduardo Kraus <kraus@eduardokraus.com>
2025-01-16 13:36:29 -0300

    Buttons

--- amd/src/paste.js
+++ amd/src/paste.js
@@ -28,7 +28,7 @@ define(["jquery"], function($) {
                 var $coursesection = $(courseSection);
 
                 var $button = $coursesection.find("[data-action=open-chooser]");
-                var sectionId = $button.attr("data-sectionnum");
+                var sectionId = $button.attr("data-sectionnum") || $button.attr("data-sectionid") || $button.attr("data-sectionreturnid");
                 var beforeModule = $button.attr("data-beforemod");
                 if (!beforeModule) {
                     beforeModule = "";
@@ -37,14 +37,38 @@ define(["jquery"], function($) {
                 var urlreturn = encodeURIComponent(location.href.replace(M.cfg.wwwroot, ""));
                 var url = `${M.cfg.wwwroot}/local/copy/paste.php?courseid=${courseid}&section=${sectionId}` +
                     `&beforemodule=${beforeModule}&returnurl=${urlreturn}`;
-                $coursesection.find(".divider-content")
-                    .addClass("d-flex")
-                    .addClass("align-items-center")
-                    .append(`<form action="${url}"
-                                   method="post">
-                                 <button class="btn add-content local_copy-paste-content p-1"
-                                         type="submit">${pastetext}</button>
-                             </form>`);
+
+                $coursesection
+                    .addClass("d-flex align-items-center justify-content-center")
+                    .append(`
+                        <form action="${url}"
+                              method="post">
+                            <button class="btn add-content local_copy-paste-content p-1"
+                                    type="submit">${pastetext}</button>
+                        </form>`);
+                $coursesection.find("*").css({position: "initial"});
+                $coursesection.find("button").css({
+                    borderRadius: "10px",
+                    width: "auto",
+                });
+            });
+
+            $(".section-modchooser.activity-add.bulk-hidden").each(function(id, courseSection) {
+                var $coursesection = $(courseSection);
+
+                var sectionId = $coursesection.attr("data-sectionnum") || $coursesection.attr("data-sectionid") || $coursesection.attr("data-sectionreturnid");
+
+                var urlreturn = encodeURIComponent(location.href.replace(M.cfg.wwwroot, ""));
+                var url = `${M.cfg.wwwroot}/local/copy/paste.php?courseid=${courseid}&section=${sectionId}` +
+                    `&returnurl=${urlreturn}`;
+
+                $coursesection.before(`
+                        <form action="${url}"
+                              method="post">
+                            <button class="btn add-content local_copy-paste-content p-1"
+                                    type="submit">${pastetext}</button>
+                        </form>`);
+                $coursesection.find("*").css({position: "initial"});
             });
         }
     };
