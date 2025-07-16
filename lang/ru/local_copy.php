df376a8707ea545c8322fa83c38fe36d847dbaa2
CatSema <catsemaofficial@gmail.com>
2025-07-16 22:25:46 +0300

    feat: add Russian language support

---
+++ lang/ru/local_copy.php
@@ -0,0 +1,36 @@
+<?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
+
+
+/**
+ * lang ru file
+ *
+ * @package   local_copy
+ * @copyright 2024 Eduardo Kraus {@link http://eduardokraus.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+
+$string['copy'] = 'Копировать модуль в буфер обмена';
+$string['copy:manage'] = 'Копирование модулей';
+$string['copyederror'] = 'Ошибка копирования';
+$string['copyedsuccess'] = 'Скопировано успешно';
+$string['modulename'] = 'Копирование модулей';
+$string['pasteempty'] = 'В настоящее время в буфере обмена нет доступных модулей.';
+$string['pastehere'] = 'Вставить <strong>{$a}</strong> здесь';
+$string['pastesuccess'] = 'Вставлено успешно';
+$string['pluginname'] = 'Копирование модулей';
+$string['privacy:metadata'] = 'Этот плагин не собирает, не хранит и не передает никаких персональных данных студентов.';
