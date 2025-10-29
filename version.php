6483a99ff2c9aacf84bfed00b894a023cbf9e574
Eduardo Kraus <kraus@eduardokraus.com>
2025-10-29 19:20:01 -0300

    V 1.1.12

--- version.php
+++ version.php
@@ -25,7 +25,7 @@
 defined('MOODLE_INTERNAL') || die;
 
 $plugin->version   = 2025102900;
+$plugin->release   = "1.1.12";
 $plugin->requires  = 2021041900;
-$plugin->release   = "1.1.11";
 $plugin->maturity  = MATURITY_STABLE;
 $plugin->component = "local_copy";
