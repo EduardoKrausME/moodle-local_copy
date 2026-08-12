f9e2bcf87aaf1bb3d00375dccec5d4bec67428d5
Eduardo Kraus <kraus@eduardokraus.com>
2026-08-12 14:59:01 -0300

    update copy and paste language strings

--- lang/pt_br/local_copy.php
+++ lang/pt_br/local_copy.php
@@ -1,41 +1,63 @@
 <?php
+// This file is part of Moodle - http://moodle.org/
+//
+// Moodle is free software: you can redistribute it and/or modify
+// it under the terms of the GNU General Public License as published by
+// the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+//
+// Moodle is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with Moodle.  If not, see <http://www.gnu.org/licenses/>.
 
+/**
+ * lang en file
+ *
+ * @package   local_copy
+ * @copyright 2024 Eduardo Kraus {@link https://eduardokraus.com}
+ * @license   http://www.gnu.org/copyleft/gpl.html GNU GPL v3 or later
+ */
+
+$string['clear'] = 'Limpar';
+$string['clipboardcleared'] = 'Área de transferência limpa';
+$string['clipboardfrom'] = 'Origem: {$a}';
+$string['clipboardtitlemany'] = '{$a} atividades copiadas';
+$string['clipboardtitleone'] = '1 atividade copiada';
 $string['copy'] = 'Copiar atividade para a área de transferência';
 $string['copy:manage'] = 'Copiar atividades';
 $string['copyederror'] = 'Não foi possível copiar a atividade selecionada.';
 $string['copyedsuccess'] = 'Copiado para a área de transferência';
 $string['copyedsuccessbulk'] = '{$a} atividades copiadas para a área de transferência';
 $string['copyselected'] = 'Copiar selecionadas';
+$string['invalidaction'] = 'Ação inválida para a área de transferência.';
 $string['modulename'] = 'Copiar atividades';
+$string['paste'] = 'Colar';
+$string['pastebuttonmany'] = 'Colar {$a} atividades';
+$string['pastebuttonone'] = 'Colar atividade';
 $string['pasteempty'] = 'Não há atividades na área de transferência.';
 $string['pasteerror'] = 'Nenhuma atividade foi colada. Tente novamente.';
-$string['pastesuccess'] = 'Atividade colada com sucesso';
-$string['pastesuccessbulk'] = '{$a->success} de {$a->total} atividades foram coladas';
-$string['pastepartial'] = '{$a->success} de {$a->total} atividades foram coladas. Alguns itens não puderam ser restaurados.';
-$string['pluginname'] = 'Copiar atividades';
-$string['privacy:metadata'] = 'Este plugin não coleta, armazena ou compartilha dados pessoais dos estudantes.';
-$string['clipboardtitleone'] = '1 atividade copiada';
-$string['clipboardtitlemany'] = '{$a} atividades copiadas';
-$string['clipboardfrom'] = 'Origem: {$a}';
-$string['paste'] = 'Colar';
-$string['clear'] = 'Limpar';
-$string['clipboardcleared'] = 'Área de transferência limpa';
+$string['pasteinvalidsection'] = 'A seção de destino selecionada não está mais disponível.';
+$string['pasteitemerror'] = 'Não foi possível colar esta atividade.';
 $string['pastemodaltitle'] = 'Colar atividades copiadas';
-$string['pastesection'] = 'Seção';
+$string['pastenewmodulemissing'] = 'O Moodle não retornou o identificador da atividade restaurada.';
+$string['pastepartial'] = '{$a->success} de {$a->total} atividades foram coladas. Alguns itens não puderam ser restaurados.';
 $string['pasteposition'] = 'Posição';
-$string['positionstart'] = 'No início da seção';
-$string['positionend'] = 'No final da seção';
-$string['positionbefore'] = 'Antes de "{$a}"';
-$string['positionafter'] = 'Depois de "{$a}"';
-$string['pastebuttonone'] = 'Colar atividade';
-$string['pastebuttonmany'] = 'Colar {$a} atividades';
-$string['pasting'] = 'Colando...';
-$string['sectionfallback'] = 'Seção {$a}';
+$string['pasteprecheckfailed'] = 'A validação do processo de restauração do Moodle falhou.';
+$string['pastesection'] = 'Seção';
 $string['pastesourcemissing'] = 'A atividade de origem não existe mais.';
 $string['pastesourcepermission'] = 'Você não tem mais permissão para copiar esta atividade de origem.';
-$string['pasteprecheckfailed'] = 'A validação do processo de restauração do Moodle falhou.';
-$string['pastenewmodulemissing'] = 'O Moodle não retornou o identificador da atividade restaurada.';
-$string['pasteitemerror'] = 'Não foi possível colar esta atividade.';
-$string['pasteinvalidsection'] = 'A seção de destino selecionada não está mais disponível.';
-$string['invalidaction'] = 'Ação inválida para a área de transferência.';
+$string['pastesuccess'] = 'Atividade colada com sucesso';
+$string['pastesuccessbulk'] = '{$a->success} de {$a->total} atividades foram coladas';
+$string['pasting'] = 'Colando...';
+$string['pluginname'] = 'Copiar atividades';
+$string['positionafter'] = 'Depois de "{$a}"';
+$string['positionbefore'] = 'Antes de "{$a}"';
+$string['positionend'] = 'No final da seção';
+$string['positionstart'] = 'No início da seção';
+$string['privacy:metadata'] = 'Este plugin não coleta, armazena ou compartilha dados pessoais dos estudantes.';
+$string['sectionfallback'] = 'Seção {$a}';
 $string['unexpectederror'] = 'Ocorreu um erro inesperado ao processar a área de transferência.';
